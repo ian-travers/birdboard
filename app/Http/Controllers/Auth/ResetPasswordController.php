526f775a884bbcb357cb97ef91ea5309b61c751d
Ian Travers <mr.itpe@gmail.com>
2019-07-11 14:48:20 +0300

    Switch to laravel-mix versioned assets && Set min=4 length for password validation in Reset and Register controllers

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,36 +4,42 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
-
     use ResetsPasswords;
 
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
     protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest');
+        $this->customValidateNewPassword();
+    }
+
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:4',
+        ];
+    }
+
+    private function customValidateNewPassword()
+    {
+        /**
+         * Needs: Password length >= 4 (not 8)
+         *
+         * This happens because there is a hard coded validation in
+         * @see \Illuminate\Auth\Passwords\PasswordBroker.
+         *
+         * When the reset method is called, it will always call validateReset first,
+         * which in turn calls validateNewPassword:
+         */
+        Password::validator(function ($credentials) {
+            return mb_strlen($credentials['password']) >= 4;
+        });
     }
 }
