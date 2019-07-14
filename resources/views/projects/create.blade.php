ad7dac414c91fad9a0996004261d329a3b9ef24a
Ian Travers <mr.itpe@gmail.com>
2019-07-14 15:40:05 +0300

    Create Project page

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <h1>Create a Project</h1>
+        <form method="post" action="/projects">
+            @csrf
+            
+            <div>
+                <label for="title">Title</label>
+                <input type="text" name="title" placeholder="Title">
+            </div>
+
+            <div>
+                <label for="description">Description</label>
+                <textarea name="description" rows="6"></textarea>
+            </div>
+            <button type="submit">Create Project</button>
+        </form>
+    </div>
+
+@endsection
+
