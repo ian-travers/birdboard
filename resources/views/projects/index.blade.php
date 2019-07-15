5f84c9075f58093e812b8fc0075af4e26d85211c
Ian Travers <mr.itpe@gmail.com>
2019-07-15 11:14:26 +0300

    Set w-1/3 pattern for projects index page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,12 +6,15 @@
     <div class="flex items-center mb-4">
         <a href="/projects/create">Create New Project</a>
     </div>
-    <div class="flex">
+    <div class="flex flex-wrap -mx-3">
         @forelse($projects as $project)
-            <div class="bg-white p-5 mr-4 rounded shadow w-1/3" style="height: 200px;">
-                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
-                <div class="text-gray-500">{{ Str::limit($project->description, 120) }}</div>
+            <div class="w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded shadow" style="height: 200px;">
+                    <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+                    <div class="text-gray-500">{{ Str::limit($project->description, 120) }}</div>
+                </div>
             </div>
+
         @empty
             <div>There is not projects yet.</div>
         @endforelse
