1eda7113815117d0944e153404e25d5fd88a4c96
Ian Travers <mr.itpe@gmail.com>
2019-07-15 15:50:41 +0300

    Projects index page (card border and link)

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -10,12 +10,14 @@
         </div>
     </header>
 
-    <main class="flex flex-wrap -mx-3">
+    <main class="lg:flex flex-wrap -mx-3">
         @forelse($projects as $project)
-            <div class="w-1/3 px-3 pb-6">
-                <div class="bg-white p-5 rounded shadow" style="height: 200px;">
-                    <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
-                    <div class="text-gray-500">{{ Str::limit($project->description, 120) }}</div>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px;">
+                    <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-300 pl-4 mb-3">
+                        <a href="{{ $project->path() }}">{{ $project->title }}</a>
+                    </h3>
+                    <div class="text-gray-500">{{ Str::limit($project->description, mb_strlen($project->title) < 40 ? 120 : 140 - mb_strlen($project->title)) }}</div>
                 </div>
             </div>
 
