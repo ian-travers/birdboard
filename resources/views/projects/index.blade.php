1755baecb5dc23cc76a07bd95caf435732a37aaa
Ian Travers <mr.itpe@gmail.com>
2019-07-15 16:13:30 +0300

    Modifu Project show page part 1

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <header class="flex items-center mb-3 py-3">
-        <div class="flex w-full items-center justify-between">
+        <div class="flex w-full items-end justify-between">
             <h2>My Projects</h2>
             <a class="button" href="/projects/create">Add Project</a>
         </div>
@@ -13,7 +13,7 @@
     <main class="lg:flex flex-wrap -mx-3">
         @forelse($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
-                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px;">
+                <div class="card" style="height: 200px;">
                     <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-300 pl-4 mb-3">
                         <a href="{{ $project->path() }}">{{ $project->title }}</a>
                     </h3>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,10 +3,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-3xl">{{ $project->title }}</h1>
-    <p>{{ $project->description }}</p>
-    <div style="margin-top: 1rem;">
-        <a href="/projects">Go Back</a>
-    </div>
+    <header class="flex items-center mb-3 py-3">
+        <div class="flex w-full items-end justify-between">
+            <h2>My Projects</h2>
+            <a class="button" href="/projects/create">Add Project</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 mx-3">
+                <div class="mb-6">
+                    <h2 class="text-lg text-gray-600 mb-3">Tasks</h2>
+                    {{--tasks--}}
+                    <div class="card">Lorem ipsum dolor.</div>
+                </div>
+
+                <div>
+                    <h2 class="text-lg text-gray-600 mb-3">General Notes</h2>
+                    {{--general notes--}}
+                    <div class="card">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto beatae cumque deleniti deserunt eius est illo in iure laboriosam magnam magni maiores minima minus molestiae neque nulla quae, quis quisquam vel voluptatibus. Suscipit!
+                    </div>
+                </div>
+
+            </div>
+            <div class="lg:w-1/4 mx-3">
+                <div class="card">
+                    <h1 class="text-3xl">{{ $project->title }}</h1>
+                    <p>{{ $project->description }}</p>
+                    <div style="margin-top: 1rem;">
+                        <a href="/projects">Go Back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
 @endsection
 
