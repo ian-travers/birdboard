e6878d77e30a7c022c7a39af099e12b510b8ec32
Ian Travers <mr.itpe@gmail.com>
2019-07-17 09:47:57 +0300

    Set timezone && touch Task to Project

--- app/Task.php
+++ app/Task.php
@@ -29,6 +29,8 @@ class Task extends Model
 {
     protected $guarded = [];
 
+    protected $touches = ['project'];
+
     public function project()
     {
         return $this->belongsTo(Project::class);
