129957d9d9913243b2eb1a2c161c44202edd1b39
Ian Travers <mr.itpe@gmail.com>
2019-07-23 09:24:55 +0300

    Projects resource routes

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/projects', 'ProjectsController@index');
-    Route::get('/projects/create', 'ProjectsController@create');
-    Route::get('/projects/{project}', 'ProjectsController@show');
-    Route::get('/projects/{project}/edit', 'ProjectsController@edit');
-    Route::patch('/projects/{project}', 'ProjectsController@update');
-    Route::post('/projects', 'ProjectsController@store');
-    Route::delete('/projects/{project}', 'ProjectsController@destroy');
+    Route::resource('projects', 'ProjectsController'); //TODO: use named routes over whole sources
 
     Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
     Route::patch('/projects/{project}/tasks/{task}', 'ProjectTasksController@update');
