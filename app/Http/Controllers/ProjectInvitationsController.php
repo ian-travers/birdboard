96f4025813207679603dd3442a677b73647eacd3
Ian Travers <mr.itpe@gmail.com>
2019-07-24 16:27:59 +0300

    Extract ProjectInvitationRequest

--- app/Http/Controllers/ProjectInvitationsController.php
+++ app/Http/Controllers/ProjectInvitationsController.php
@@ -2,25 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProjectInvitationRequest;
 use App\Project;
 use App\User;
 
 class ProjectInvitationsController extends Controller
 {
-    /**
-     * @param Project $project
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     */
-    public function store(Project $project)
+    public function store(Project $project, ProjectInvitationRequest $request)
     {
-        $this->authorize('update', $project);
-
-        request()->validate([
-            'email' => 'required|email|exists:users,email',
-        ], [
-            'email.exists' => 'The user you are inviting must have a Birdboard account.',
-        ]);
-
         $user = User::whereEmail(request('email'))->first();
 
         $project->invite($user);
---
+++ app/Http/Requests/ProjectInvitationRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class ProjectInvitationRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return Gate::allows('update', $this->route('project'));
+    }
+
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|exists:users,email',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.exists' => 'The user you are inviting must have a Birdboard account.',
+        ];
+    }
+}
