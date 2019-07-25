f0acf017ad1d4d51b4ba0e2291437b6d9697118b
Ian Travers <mr.itpe@gmail.com>
2019-07-25 13:22:59 +0300

    Add UI form to invitations && extract errors view

---
+++ resources/views/errors.blade.php
@@ -0,0 +1,9 @@
+@if ($errors->any())
+    <div class="field mt-6">
+
+        @foreach ($errors->all() as $error)
+            <li class="text-sm text-red-500">{{ $error }}</li>
+        @endforeach
+    </div>
+@endif
+
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -77,14 +77,8 @@
                         <button type="submit" class="button">Save</button>
                     </form>
 
-                    @if ($errors->any())
-                        <div class="field mt-6">
+                    @include('errors')
 
-                            @foreach ($errors->all() as $error)
-                                <li class="text-sm text-red-500">{{ $error }}</li>
-                            @endforeach
-                        </div>
-                    @endif
                 </div>
             </div>
 
@@ -92,6 +86,22 @@
 
                 @include('projects.card')
                 @include('projects.activity.card')
+
+                <div class="card flex flex-col mt-3">
+                    <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-300 pl-4 mb-3">
+                        Invite a User
+                    </h3>
+                    <form method="post" action="{{ $project->path() . '/invitations' }}">
+
+                        @csrf
+                        <div class="mb-3">
+                            <input type="email" name="email" class="border border-gray-400 rounded w-full py-2 px-3" placeholder="Email address">
+                        </div>
+                        <button type="submit" class="button">Invite</button>
+                    </form>
+
+                    @include('errors')
+                </div>
             </div>
         </div>
     </main>
