f68d738827c5a3a635fb01348bf101d9de00151e
Ian Travers <mr.itpe@gmail.com>
2019-07-25 09:29:09 +0300

    Add column flexibility to card widget

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,10 +1,10 @@
 @php /* @var App\Project $project */ @endphp
 
-<div class="card" style="height: 236px;">
+<div class="card flex flex-col" style="height: 236px;">
     <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-300 pl-4 mb-3">
         <a href="{{ $project->path() }}">{{ $project->title }}</a>
     </h3>
-    <div class="text-gray-500 mb-4">{{ $project->description_for_card }}</div>
+    <div class="text-gray-500 mb-4 flex-1">{{ $project->description_for_card }}</div>
     <footer>
         <form method="post" action="{{ $project->path() }}" class="text-right">
 
