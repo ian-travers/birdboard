881b584e7eb6b60c7f39ae7795c34ce91a68d48f
Ian Travers <mr.itpe@gmail.com>
2019-07-26 15:45:21 +0300

    Can switch component property

--- resources/js/components/ThemeSwitcher.vue
+++ resources/js/components/ThemeSwitcher.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="flex items-center mr-8">
-        <button class="rounded-full border border-accent w-5 h-5 mr-2"></button>
-        <button class="rounded-full border border-accent w-5 h-5 mr-2"></button>
-        <button class="rounded-full border border-accent w-5 h-5 mr-2"></button>
+        <button class="rounded-full border border-accent w-5 h-5 mr-2" @click="selectedTheme = 'theme-light'"></button>
+        <button class="rounded-full border border-accent w-5 h-5 mr-2" @click="selectedTheme = 'theme-dark'"></button>
     </div>
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+        data () {
+            return {
+                selectedTheme: 'theme-light'
+            }
+        },
     }
 </script>
