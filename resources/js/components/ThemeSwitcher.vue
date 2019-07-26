9d5b5260a4b180e494b4e19dd40f97d40f506309
Ian Travers <mr.itpe@gmail.com>
2019-07-26 16:01:05 +0300

    Switch theme on a fly (Important: change class for body element witch up from "app" selector of the Vue component)

--- resources/js/components/ThemeSwitcher.vue
+++ resources/js/components/ThemeSwitcher.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center mr-8">
-        <button class="rounded-full border border-accent w-5 h-5 mr-2" @click="selectedTheme = 'theme-light'"></button>
-        <button class="rounded-full border border-accent w-5 h-5 mr-2" @click="selectedTheme = 'theme-dark'"></button>
+        <button class="rounded-full border border-accent w-5 h-5 mr-2" style="background: #f5f6f9" @click="selectedTheme = 'theme-light'"></button>
+        <button class="rounded-full border border-accent w-5 h-5 mr-2" style="background: #222" @click="selectedTheme = 'theme-dark'"></button>
     </div>
 </template>
 
@@ -12,5 +12,11 @@
                 selectedTheme: 'theme-light'
             }
         },
+
+        watch: {
+            selectedTheme() {
+                document.body.className = document.body.className.replace(/theme-\w+/, this.selectedTheme);
+            }
+        }
     }
 </script>
