719a4dde7e1607c3f7a19d3b1486ed9618b0219e
Ian Travers <mr.itpe@gmail.com>
2019-07-29 14:41:51 +0300

    Drop unused js packages. Remove unused color sets.

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,7 +1,7 @@
 @php /* @var App\Project $project */ @endphp
 
 <div class="card flex flex-col" style="height: 236px;">
-    <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-300 pl-4 mb-3">
+    <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-accent pl-4 mb-3">
         <a href="{{ $project->path() }}" class="text-default">{{ $project->title }}</a>
     </h3>
     <div class="text-default mb-4 flex-1">{{ $project->description_for_card }}</div>
