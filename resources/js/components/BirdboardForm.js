d7d33945122d450f9a33e2389abc590577b07edc
Ian Travers <mr.itpe@gmail.com>
2019-07-29 11:31:32 +0300

    OOP JS Part 1. Create form object. Validate data. Post data.

---
+++ resources/js/components/BirdboardForm.js
@@ -0,0 +1,35 @@
+class BirdboardForm {
+
+    constructor(data) {
+        this.originalData = JSON.parse(JSON.stringify(data));
+
+        Object.assign(this, data);
+
+        this.errors = {};
+    }
+
+    data() {
+        let data = {};
+
+        for (let attribute in this.originalData) {
+            data[attribute] = this[attribute];
+        }
+
+        return data;
+    }
+
+    submit(endpoint) {
+        return axios.post(endpoint, this.data())
+            .catch(this.onFail.bind(this));
+    }
+
+    onFail(error) {
+        this.errors = error.response.data.errors;
+    }
+
+    reset() {
+        Object.assign(this, this.originalData);
+    }
+}
+
+export default BirdboardForm;
